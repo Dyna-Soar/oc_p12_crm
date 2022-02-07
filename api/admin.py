f1657ec4ef0da20d54e6316cbbf04be2f05200b5
Bodin <t.bodin@MacBook-Air-de-Bodin.local>
2022-02-07 04:07:20 +0100

    add models to admin register

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, SalesEmployee, SupportEmployee, Prospect, Client, Contract, Event
 
 # Register your models here.
+
+admin.site.register(User, UserAdmin)
+admin.site.register(SalesEmployee)
+admin.site.register(SupportEmployee)
+admin.site.register(Prospect)
+admin.site.register(Client)
+admin.site.register(Contract)
+admin.site.register(Event)
