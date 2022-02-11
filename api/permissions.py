a1cae91f222cba6f8e38dc5bb886d127d9da5ce6
Bodin <t.bodin@MacBook-Air-de-Bodin.local>
2022-02-12 00:25:18 +0100

    add permissions

--- api/permissions.py
+++ api/permissions.py
@@ -1,17 +1,116 @@
-from rest_framework.permissions import BasePermission
-
 from rest_framework import permissions
 
-from .models import User, SalesEmployee, SupportEmployee, Prospect, Client, Contract, Event
+from .models import User, Manager, SalesEmployee, SupportEmployee, Prospect, Client, Contract, Event
 
 
-class IsGetOrIsAuthenticated(BasePermission):
-
+class IsManager(permissions.BasePermission):
     def has_permission(self, request, view):
-        # allow all GET requests
-        if request.method == 'GET':
+        return Manager.objects.filter(user=request.user).exists()
+
+
+class IsSales(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return SalesEmployee.objects.filter(user=request.user).exists()
+
+
+class IsSupport(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return SupportEmployee.objects.filter(user=request.user).exists()
+
+
+class IsSupportEvent(permissions.BasePermission):
+    def has_permission(self, request, view):
+        event_id = request.path.split("/")[2]
+        event = Event.objects.get(id=event_id)
+        if event in SupportEmployee.objects.get(user=request.user).events:
             return True
 
-        # Otherwise, only allow authenticated requests
-        # return request.user and request.user.is_authenticated
-        return request.user.is_authenticated
+
+class PermissionManager(permissions.BasePermission):
+    """
+    GET: Only managers
+    POST: Only managers
+    """
+
+
+class PermissionSpecificManager(permissions.BasePermission):
+    """
+    GET: Only managers
+    PUT: Only managers
+    """
+
+
+class PermissionSalesEmployee(permissions.BasePermission):
+    """
+    GET: Only managers
+    POST: Only managers
+    """
+
+
+class PermissionSpecificSalesEmployee(permissions.BasePermission):
+    """
+    GET: The specific sales employee and managers
+    PUT: Only managers
+    """
+
+
+class PermissionSupportEmployee(permissions.BasePermission):
+    """
+    GET: Only managers
+    POST: Only managers
+    """
+
+
+class PermissionSpecificSupportEmployee(permissions.BasePermission):
+    """
+    GET: The specific support employee and managers
+    PUT: Only managers
+    """
+
+
+class PermissionClient(permissions.BasePermission):
+    """
+    GET: Sales employees and managers
+    POST: Sales employees and managers
+    """
+
+
+class PermissionSpecificClient(permissions.BasePermission):
+    """
+    GET: Sales employees and managers
+    PUT: Sales employees and managers
+    """
+
+
+class PermissionContract(permissions.BasePermission):
+    """
+    GET: Sales employees and managers
+    POST: Sales employees and managers
+    """
+
+
+class PermissionSpecificContract(permissions.BasePermission):
+    """
+    GET: Sales employees and managers
+    PUT: Sales employees and managers
+    """
+
+
+class PermissionEvent(permissions.BasePermission):
+    """
+    GET: Sales employees and managers
+    POST: Sales employees and managers
+    """
+
+
+class PermissionSpecificEvent(permissions.BasePermission):
+    """
+    GET: Sales employees, associated support employee and managers
+    PUT: Associated support employee and managers
+    """
+
+
+class PermissionEventClient(permissions.BasePermission):
+    """
+    GET: Sales employees, associated support employee and managers
+    """
